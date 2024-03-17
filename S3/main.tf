aa3e90bd8c2f06efbf287a2a9e43c8b7e46aeb7b
Shubham <rshubham228@gmail.com>
2024-03-17 10:12:35 +0530

    3

---
+++ S3/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0" 
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "terrateam-bucket01" {
+  type    = string
+  default = "terrateam-dev-bucket-trial2"
+
+}
+
+resource "aws_s3_bucket" "terrateam-bucket081" {
+  bucket = var.terrateam-bucket01
+}
